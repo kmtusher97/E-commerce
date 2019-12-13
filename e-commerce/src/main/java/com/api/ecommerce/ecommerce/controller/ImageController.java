e3a9dbadce748c5c43eab3bfeca67d4bc13e5087
kmtusher97 <kmtusher97@gmail.com>
2019-12-14 01:17:06 +0600

    Add image Post request

--- e-commerce/src/main/java/com/api/ecommerce/ecommerce/controller/ImageController.java
+++ e-commerce/src/main/java/com/api/ecommerce/ecommerce/controller/ImageController.java
@@ -1,7 +1,6 @@
 package com.api.ecommerce.ecommerce.controller;
 
 import com.api.ecommerce.ecommerce.model.Image;
-import com.api.ecommerce.ecommerce.model.Product;
 import com.api.ecommerce.ecommerce.service.ImageServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,8 +15,8 @@ public class ImageController {
     @Autowired
     private ImageServices imageServices;
 
-   /* @PostMapping("/add")
-    public Image addImage(@RequestBody Product product) {
-
-    }*/
+    @PostMapping("/add")
+    public Image addImage(@RequestBody Image image) {
+        return imageServices.saveOrUpdateImage(image);
+    }
 }
