a06b0a769c0c6ac01595d8e50b9bb198c0b1f633
kmtusher97 <kmtusher97@gmail.com>
2019-12-14 01:57:48 +0600

    Product RestController

--- e-commerce/src/main/java/com/api/ecommerce/ecommerce/controller/ProductController.java
+++ e-commerce/src/main/java/com/api/ecommerce/ecommerce/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.api.ecommerce.ecommerce.controller;
 import com.api.ecommerce.ecommerce.model.Product;
 import com.api.ecommerce.ecommerce.service.ProductServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,13 +14,30 @@ public class ProductController {
     @Autowired
     private ProductServices productServices;
 
+
+    /**
+     * @return
+     */
     @GetMapping("/all")
     public List<Product> getAllProducts() {
         return productServices.getAllProducts();
     }
 
-    @GetMapping("/{productId}")
-    public Product getProductById(@PathVariable("productId") Long productId) {
+    /**
+     * @param productId
+     * @return
+     */
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable("id") Long productId) {
         return productServices.getProductById(productId);
+    }
+
+    /**
+     * @param product
+     * @return
+     */
+    @PostMapping("/add")
+    public Product addNewProduct(@RequestBody Product product) {
+        return productServices.saveOrUpdateProduct(product);
     }
 }
