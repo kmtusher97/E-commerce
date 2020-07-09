7508c94bf99189692cf72664d7fc0473b6b46a3f
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2020-07-09 20:06:26 +0600

    Add template

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import home_page
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home_page),
+    path('home/', home_page),
+    path('about/', home_page),
+    path('contact/', home_page),
+    path('admin/', admin.site.urls)
 ]
